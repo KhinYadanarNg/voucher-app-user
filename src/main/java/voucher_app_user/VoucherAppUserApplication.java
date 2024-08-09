6bdf9a0e4fd4208c386665e1d0f8e65a6a88ebee
KhinYadanarNg <e0015793@u.nus.edu>
2024-08-09 23:07:33 +0800

    initial project setup

---
+++ src/main/java/voucher_app_user/VoucherAppUserApplication.java
@@ -0,0 +1,13 @@
+package voucher_app_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherAppUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherAppUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/voucher_app_user/VoucherAppUserApplicationTests.java
@@ -0,0 +1,13 @@
+package voucher_app_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherAppUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
